4005ece3d8a8ad8a1ae36573483959313c70228f
Colin Bruin <colinjb23@gmail.com>
2023-04-10 14:03:24 -0700

    adding a users component through cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       database: '',
       entities: ['entities/*.entity.ts'],
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
