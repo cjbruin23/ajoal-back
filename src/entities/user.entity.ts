7e54e9446d17572d36588dd5b6e61a8effb00bdf
Colin Bruin <colinjb23@gmail.com>
2023-04-10 13:52:18 -0700

    adding types to the columns of user

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -2,9 +2,9 @@ import { Column, Entity, PrimaryColumn } from 'typeorm';
 
 @Entity()
 export class User {
-  @PrimaryColumn()
+  @PrimaryColumn({ type: 'string', length: 20 })
   id: string;
 
-  @Column()
+  @Column({ type: 'string' })
   fullname: string;
 }
