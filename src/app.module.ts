f9625777be60d4f45dd473c71a8ccfc487f207b1
Colin Bruin <colinjb23@gmail.com>
2023-04-10 13:47:20 -0700

    adding user entity and connection logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: '',
+      entities: ['entities/*.entity.ts'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,19 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const audience = process.env.AUDIENCE;
   const issuerBaseURL = process.env.ISSUER_BASE_URL;
-  console.log(audience, issuerBaseURL);
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
     origin: ['http://127.0.0.1:5173'],
   });
+
   app.use(
     auth({
       issuerBaseURL,
       audience,
     }),
   );
+
   await app.listen(3000);
 }
 bootstrap();
