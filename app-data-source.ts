b79957c03a6232a331bca732a1146daf1b59b400
Colin Bruin <colinjb23@gmail.com>
2023-05-05 08:23:54 -0700

    sucessfully running the migrations

--- app-data-source.ts
+++ app-data-source.ts
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// We do not want synchronize true for Production
 const myDataSource = new DataSource({
   type: "postgres",
   host: process.env.DB_HOST,
@@ -11,10 +10,9 @@ const myDataSource = new DataSource({
   database: "postgres",
   username: "postgres",
   password: process.env.DB_PASSWORD,
-  entities: [__dirname + "dist/src/database/entity/*.js"],
-  migrations: [__dirname + "src/database/migrations"],
+  entities: ["dist/src/database/entity/*.js"],
+  migrations: ["dist/src/database/migrations/*.js"],
   logging: true,
-  synchronize: true,
 });
 
 export default myDataSource;
--- src/database/entity/user.entity.ts
+++ src/database/entity/user.entity.ts
@@ -5,7 +5,7 @@ export class User {
   @PrimaryGeneratedColumn()
   id!: number;
 
-  @Column()
+  @Column({ nullable: true })
   auth0id!: string;
 
   @Column()
