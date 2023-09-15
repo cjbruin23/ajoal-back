7fe9351fe176d0609e96ae8bbe040c86d5e7ab61
cjbruin23 <colinjb23@gmail.com>
2023-09-14 17:35:14 -0700

    small refactor

--- src/routes/questions.ts
+++ src/routes/questions.ts
@@ -7,30 +7,28 @@ import UserService from "../database/repositories/users.service";
 const router = express.Router({ mergeParams: true });
 
 router.get("/", async (req: Request, res: Response) => {
-  console.log("get all", req.params);
   const questionsService = new QuestionService(myDataSource);
-  const questionResults = await questionsService.getAllQuestionsForUser(+req.params.userId)
+  const questionResults = await questionsService.getAllQuestionsForUser(+req.params.userId);
   res.status(200).json(questionResults);
 });
 
 router.post("/", async (req: Request, res: Response, next: NextFunction) => {
-  console.log("request", req);
   try {
     const questionService = new QuestionService(myDataSource);
     const userService = new UserService(myDataSource);
     const routeParams = req.params;
-    const userId = routeParams.userId;
+    const userId = +routeParams.userId;
     const body = req.body as QuestionRequest;
-    console.log("body", body);
+
     const saveResult = await questionService.saveQuestion(
-      +userId,
+      userId,
       body,
       userService,
       next
     );
     res.send(`OK ${saveResult}`);
   } catch (err) {
-    console.log("controller err", err);
+    console.error("controller err", err);
     res.status(400).send("Invalid fields");
   }
 });
