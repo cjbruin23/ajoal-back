cd7e1876126f21c931276b237c93d986f1c66bf1
cjbruin23 <colinjb23@gmail.com>
2023-12-21 11:53:33 -0800

    adding sequelize

--- index.ts
+++ index.ts
@@ -1,15 +1,34 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { Sequelize } from "sequelize";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+const sequelize = new Sequelize(`${process.env.POSTGRES_URL}`);
+
+authenticateDbConnection();
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
+app.get('/user', (req: Request, res: Response) => {
+  res.send('You are looking for user')
+})
+
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
+
+async function authenticateDbConnection() {
+
+  try {
+    await sequelize.authenticate();
+    console.log("success in connection with DB")
+  } catch (error) {
+    console.error('Unable to connect to database', error)
+  }
+}
