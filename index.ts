e4df806838dc2f07ce1df8a692262bb7807ec003
cjbruin23 <colinjb23@gmail.com>
2023-12-21 15:39:34 -0800

    switching to typeorm after finding difficulty syncing typescript and sequelize together

--- index.ts
+++ index.ts
@@ -1,25 +1,17 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import { authenticateDbConnection, createDbFunction } from "./utils/db";
-import syncModels from "./models";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-const sequelize = createDbFunction();
-authenticateDbConnection(sequelize);
-(async () => {
- await syncModels(sequelize);
-})()
-
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
 app.get('/user', (req: Request, res: Response) => {
-  console.log('User',)
+
   res.send('You are looking for user');
 
 })
