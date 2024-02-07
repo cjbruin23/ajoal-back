7ef223cf7aab2a0ec813aa5f19f8b148f42e032a
cjbruin23 <colinjb23@gmail.com>
2024-02-06 16:43:17 -0800

    replacing gin with chi

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "periate-back/main/routes"
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
 
 func main() {
-	routes.Run()
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"http://localhost:5173"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+	}))
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
+	http.ListenAndServe(":3000", r)
 }
